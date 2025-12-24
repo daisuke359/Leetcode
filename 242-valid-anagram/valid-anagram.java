6ae2aa2905142ce77acc02a23ceb9e1c4a096809
Daisuke Kawabuchi <77464668+daisuke359@users.noreply.github.com>
2025-12-24 14:40:52 +0900

    Time: 25 ms (6.90%) | Memory: 47 MB (7.11%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length()!=t.length()) {
+            return false;
+        }
+        HashMap<Character,Integer> mapS = new HashMap<>();
+        HashMap<Character,Integer> mapT = new HashMap<>();
+
+        for(int i=0;i<s.length(); i++) {
+            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i), 0)+1);
+            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0)+1);
+        }
+
+        for(int j=0; j<s.length(); j++) {
+            if(!mapS.get(s.charAt(j)).equals(mapT.get(s.charAt(j)))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
