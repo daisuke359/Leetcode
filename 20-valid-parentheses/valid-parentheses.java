34d51cd7d11fb58228e01979037c15f965c3d6dd
Daisuke Kawabuchi <77464668+daisuke359@users.noreply.github.com>
2026-07-03 23:27:30 +0900

    Time: 4 ms (38.72%) | Memory: 43.7 MB (10.41%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,26 +1,48 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
         for(int i=0;i<s.length();i++) {
-            if(s.charAt(i)=='(') {
-                stack.push(')');
-            }else if(s.charAt(i)=='{') {
-                stack.push('}');
-            } else if(s.charAt(i)=='[') {
-                stack.push(']');
-            } else {
-                if(stack.isEmpty() || s.charAt(i)!=stack.peek()) {
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='[') {
+                stack.push(s.charAt(i));
+            }
+
+            if(s.charAt(i)==')' || s.charAt(i)=='}' || s.charAt(i)==']') {
+                if(stack.size()==0) {
                     return false;
-                } else {
-                    stack.pop();
+                }
+                if(s.charAt(i)==')') {
+                    if(stack.peek()=='(') {
+                        stack.pop();
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+
+                else if(s.charAt(i)=='}') {
+                    if(stack.peek()=='{') {
+                        stack.pop();
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+
+                else if(s.charAt(i)==']') {
+                    if(stack.peek()=='[') {
+                        stack.pop();
+                        continue;
+                    } else {
+                        return false;
+                    }
                 }
             }
+            
         }
 
-        if(stack.isEmpty()) {
+        if(stack.size()==0) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
