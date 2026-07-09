91e29631467b5a93757be7883e8955b61366ae96
Daisuke Kawabuchi <77464668+daisuke359@users.noreply.github.com>
2026-07-09 21:12:23 +0900

    Time: 0 ms (100.00%) | Memory: 42.9 MB (33.37%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,14 +15,13 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-
         if(p==null && q==null) {
             return true;
         } else if(p==null || q==null) {
             return false;
         }
 
-        if(p.val != q.val) {
+        if(p.val!=q.val) {
             return false;
         }
 
